fe09f9f7bf5594f188600eb17c9576c1c5f8a6ce
navalrahman <77826561+navalrahman@users.noreply.github.com>
2025-01-16 19:49:27 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,14 +4,23 @@ import Admin from "../models/admin/adminModel.js";
 
 
 const protect = async (req, res, next) => {
-    let { authorization } = req.headers
+    // let { authorization } = req.headers
+    // console.log("tokesssn", req.headers);
+
+    // if (!authorization) {
+    //     return res.status(401).json({ error: "Unauthorized" });
+    // }
+
+    // const token = authorization.split(' ')[1];
+    
+    let { authorization , Authorization } = req.headers
     console.log("tokesssn", req.headers);
 
-    if (!authorization) {
+    if (!authorization || !Authorization) {
         return res.status(401).json({ error: "Unauthorized" });
     }
 
-    const token = authorization.split(' ')[1];
+    const token = authorization.split(' ')[1] || Authorization.split(' ')[1]
 
 
     try {
